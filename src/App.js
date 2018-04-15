e8177146f5ca813bddd2447c42ebd85e27c71308
Josh Humpherys <joshua.humpherys@gmail.com>
2018-04-14 20:32:42 -0400

    Add board

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import battleshipLogo from './battleship.svg'
 import { Button, Input } from 'semantic-ui-react'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      code: null,
+      playing: false,
+      starting: false,
+      gridSize: 10,
+    };
+
+    this.joinGame = this.joinGame.bind(this);
+    this.createGame = this.createGame.bind(this);
+    this.startGame = this.startGame.bind(this);
+  }
+
+  joinGame() {
+    this.setState({ playing: true });
+  }
+
+  createGame() {
+    this.setState({ starting: true });
+  }
+
+  startGame() {
+    this.setState({ starting: false, playing: true });
+  }
+
   render() {
     return (
       <div className="app">
@@ -10,11 +37,46 @@ class App extends Component {
           <img src={battleshipLogo} className="logo" alt="logo" />
           <h1 className="intro">Welcome to Multiplayer Battleship</h1>
         </header>
-        <div className="body">
-          <Input placeholder='Game name' />
-          <Button>Join game</Button>
-          <Button>Create game</Button>
-        </div>
+        {
+          this.state.playing ? (
+            <table className="board">
+              <thead>
+                <tr>
+                  {
+                    new Array(this.state.gridSize + 1).fill().map((_, i) =>
+                      <td>
+                        {i === 0 ? '' : String.fromCharCode('A'.charCodeAt(0) + i - 1)}
+                      </td>
+                    )
+                  }
+                </tr>
+              </thead>
+              {
+                new Array(this.state.gridSize).fill('').map((_, i) =>
+                  <tr key={i}>
+                    {
+                      new Array(this.state.gridSize + 1).fill('').map((_, j) =>
+                        j === 0 ? <td>{i + 1}</td> : <td />
+                      )
+                    }
+                  </tr>
+                )
+              }
+            </table>
+          ) : (
+            this.state.starting ? (
+              <div className="body">
+                <Button onClick={this.startGame}>Start game</Button>
+              </div>
+            ) : (
+              <div className="body">
+                <Input placeholder='Game name' onChange={(e, data) => this.setState({ code: data.value })} />
+                <Button onClick={this.joinGame}>Join game</Button>
+                <Button onClick={this.createGame}>Create game</Button>
+              </div>
+            )
+          )
+        }
       </div>
     );
   }
